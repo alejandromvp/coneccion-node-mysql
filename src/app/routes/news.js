cd8803ed1d915ec9490d07f2b33424eb9df4c860
Alejandro villagran <alejandro18maestro@gmail.com>
2018-05-29 16:31:09 -0400

    terminado

--- src/app/routes/news.js
+++ src/app/routes/news.js
@@ -1,5 +1,25 @@
+const dbConnection = require('../../config/dbConnection')
+
 module.exports = app => {
-    app.get('/', (req, res) => {
-        res.send("hello world")
-    })
+    const connection = dbConnection()
+
+    app.get('/news', (req, res) => {
+        connection.query('SELECT * FROM news', (err, result)=>{
+            res.render('news/news', {
+                news: result
+            });
+        });  
+    });
+
+    app.post('/news', (req, res) => {
+        const { title, news } = req.body;
+        connection.query('INSERT INTO news SET ? ',
+          {
+            title,
+            news
+          }
+        , (err, result) => {
+          res.redirect('/news');
+        });
+    });
 }
--- src/config/server.js
+++ src/config/server.js
@@ -7,7 +7,7 @@ const app = express();
 //settings
 app.set('port', process.env.PORT || 3000);
 app.set('view engine', 'ejs');
-app.set('view', path.join(__dirname, '../app/views'));
+app.set('views', path.join(__dirname, '../app/views'));
 
 //middleware
 app.use(bodyParser.urlencoded({extended: false}));
